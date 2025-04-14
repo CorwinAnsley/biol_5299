1ac92ea1ad2a7f640d6ae9d4f8ac945f09a0f53b
CorwinAnsley <coreyansley@gmail.com>
2025-04-14 15:22:36 +0100

    created process_data.sh

---
+++ process_data.sh
@@ -0,0 +1,64 @@
+#!/bin/bash -l
+
+############# SLURM SETTINGS #############
+
+#SBATCH --account=project0026 # the project account - don't change this
+#SBATCH --job-name=process_data # give your job a name!
+#SBATCH --partition=nodes # we are using CPU nodes - don't change this
+#SBATCH --time=0-04:00:00 # how long do we expect this job to run?
+#SBATCH --mem=4G # how much memory do we want?
+#SBATCH --nodes=1 # how many nodes do we want?
+#SBATCH --ntasks=1 # how many tasks are we submitting?
+#SBATCH --cpus-per-task=1 # how many CPUs do we want for each task?
+#SBATCH --ntasks-per-node=1 # how many tasks do we want to run on each node?
+#SBATCH --mail-user=2266643a@student.gla.ac.uk # email address for notifications
+#SBATCH --mail-type=END # mail me when my jobs ends
+#SBATCH --mail-type=FAIL # mail me if my jobs fails
+#SBATCH --reservation=bioinf_course #NOTE: this will only work between 14th and 19th April - remove this line after 19th April!
+
+
+############# CODE #############
+raw_data=../pathogenPolyomicsData
+data=./data
+outputs=./outputs
+fastqc_output=$outputs/fastqc
+reference=./data/reference/reference
+
+# Index the reference genome
+bowtie2-build ${data}/Reference/TriTrypDB-25_LmexicanaMHOMGT2001U1103.fa $reference
+
+for sample in LmexWT LmexAmpB 
+
+do
+    sam="${data}/${sample}.sam"  # path to write/read bowtie2 SAM file
+    bam="${data}/${sample}.bam"  # path to write/read  samtools-converted BAM file 
+    sorted_bam="${data}/${sample}.sort.bam"	 # path to write/read  samtools-sorted BAM file
+    bai="${data}/${sample}.bai"
+
+    for pair in 1 2
+    do
+        # Define filepaths
+        fastq="${raw_data}/DNAseq/${sample}_${pair}.fastq.gz"
+
+        # Generate fastqc report
+        fastqc -o $fastqc_output --svg  -f fastq $fastq
+    done
+
+    # Carry out trimming
+    trim_galore --phred64 --illumina --paired -q 20 -o ${data}/ --basename $sample fastq="${raw_data}/DNAseq/${sample}_1.fastq.gz" fastq="${raw_data}/DNAseq/${sample}_2.fastq.gz"
+
+    # Carry out alignment to reference with bowtie2
+    bowtie2 --phred64  -x $reference -1 ${sample}_val_1.fq.gz -2 ${sample}_val_1.fq.gz -S $sam
+
+    # Convert sam to bam with samtools
+    samtools view -b -o ${bam} ${sam}
+
+    # With samtools, sort bam
+    samtools sort -o ${sorted_bam} ${bam}
+
+    # Index alignment file with samtools
+    samtools index --bai -o $bai $sorted_bam
+
+done 
+
+
